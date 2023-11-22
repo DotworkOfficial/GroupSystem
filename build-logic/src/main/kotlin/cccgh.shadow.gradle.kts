7b299492ce3cdae9e47ae744d175dc5073b58eb5
ElroyKR <elroykr@gmail.com>
2023-11-22 16:06:31 +0900

    Update build logic for debugging

--- build-logic/src/main/kotlin/cccgh.shadow.gradle.kts
+++ build-logic/src/main/kotlin/cccgh.shadow.gradle.kts
@@ -4,6 +4,12 @@ plugins {
 }
 
 tasks.shadowJar {
+    if (project.rootProject.hasProperty("debugOutputDir")) {
+        destinationDirectory.set(file("${project.rootProject.properties["debugOutputDir"]}"))
+    } else {
+        destinationDirectory.set(file(rootProject.projectDir.path + "/build_outputs"))
+    }
+
     archiveBaseName.set(project.rootProject.name)
     archiveVersion.set("")
     archiveClassifier.set("")
