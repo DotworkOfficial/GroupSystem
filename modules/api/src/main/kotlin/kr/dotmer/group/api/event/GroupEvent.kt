6d0defec6bdc03a5e4e5c1c01e14586548ff4b3b
ElroyKR <elroykr@gmail.com>
2023-11-28 16:31:10 +0900

    fix: Change GroupEvent to async event

--- modules/api/src/main/kotlin/kr/dotmer/group/api/event/GroupEvent.kt
+++ modules/api/src/main/kotlin/kr/dotmer/group/api/event/GroupEvent.kt
@@ -6,7 +6,7 @@ import org.bukkit.event.HandlerList
 
 abstract class GroupEvent<T : BaseGroup>(
     val group: T
-) : Event() {
+) : Event(true) {
     companion object {
         private val HANDLERS = HandlerList()
 
